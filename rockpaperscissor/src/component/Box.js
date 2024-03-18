45fd621d0c8906763f96c57de53f97fce5345cde
anjuhim <zzonzzon84@nate.com>
2024-03-18 22:47:54 +0900

    img에 alt추가

--- rockpaperscissor/src/component/Box.js
+++ rockpaperscissor/src/component/Box.js
@@ -1,11 +1,10 @@
 import React from 'react';
 
 const Box = (props) => {
-  
   return (
     <div className="box">
       <h1>{props.title}</h1>
-      <img className="item-img" src={props.item && props.item.img} />
+      <img className="item-img" src={props.item && props.item.img} alt="" />
       <h2>win</h2>
     </div>
   );
